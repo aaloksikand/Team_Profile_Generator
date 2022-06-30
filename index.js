c400657750870d37e1bc32f7c96cd7cd21550158
aaloksikand <aalok.sikand@gmail.com>
2022-06-29 20:45:07 -0700

    Added bootstrap and javascript code to render HTML file

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
 
-import inquirer from 'inquirer'; //Importing inquirer according to updated version of inquirer requirements
-
-import fs from 'fs'; //Importing fs
-import CheckboxPrompt from 'inquirer/lib/prompts/checkbox';
-const renderTeam = require("./src/bootstrap")
+const inquirer = require("inquirer"); //Importing inquirer according to updated version of inquirer requirements
+const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Inter");
+const renderTeam = require("./src/bootstrap");
 
 function init() // WHEN I start the application
 {
@@ -87,9 +72,9 @@ function addEmployees() {
 })
 function createEngineer() {
 
-  inquirer  //using the inqurer prompt
+  inquirer  
   .prompt([
-    {  // THEN I am prompted to enter the engineer’s name . . .
+    {  
       type: 'input',  
       message: 'Please enter the team Engineer name.',
       name: 'name',  
@@ -126,27 +111,27 @@ function createEngineer() {
 
 function createIntern() {
 
-  inquirer  //using the inqurer prompt
+  inquirer 
   .prompt([
-    {  // THEN I am prompted to enter the team manager’s name . . .
+    {  
       type: 'input',  
       message: 'Please enter the team Intern name.',
       name: 'name',  
     },
     {
-      type: 'input', //user input for Description
-      message: 'Please enter the Intern employee ID.',  //See Acceptance Criteria: "// WHEN I enter a description . . ."
+      type: 'input', 
+      message: 'Please enter the Intern employee ID.',  
       name: 'id',
     },
     {
-      type: 'input',  //user input for Installation Instructions
+      type: 'input',  
       message: 'Please enter the Intern email address.',
-      name: 'email', //See Acceptance Criteria: ". . . WHEN I enter installation instructions . . .""
+      name: 'email', 
       },
     {
-      type: 'input',  //user input for project usage
+      type: 'input',
       message: 'Please enter Intern school of attendance.',
-      name: 'github',  // WHEN I enter . . . usage information . . .""
+      name: 'github',  
       },
   ])
 
@@ -159,16 +144,11 @@ function createIntern() {
     )
     teamMemberObjArr.push(Intern)
     addEmployees()
-
 })
 }
  function buildTeam() {
   fs.writeFile("./dist/index.html", renderTeam(teamMemberObjArr), "utf-8")
  }
-)
-})
 }
-
-
-
+}
 init();
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,8 @@
+const Employee = require('./Employee');
+const Manager = require('./Manager');
+const Engineer = require('./Engineer');
+const Intern = require('./Intern');
+
 const buildTeam = team => {
 
     const generateManagerCard = manager => {
@@ -7,8 +12,8 @@ const buildTeam = team => {
       Team Manager
     </div>
     <div class="card-body">
-      <h5 class="card-title">${manager.email}</h5>
-      <p class="card-text">Office Number</p>
+      <h5 class="card-title">${manager.name}</h5>
+      <p class="card-text">${manager.officeNumber}</p>
       <a href="mailto:${manager.email}" class="btn btn-primary">Send Email</a>
     </div>
     <div class="card-footer text-muted">
@@ -23,5 +28,70 @@ const buildTeam = team => {
     .map(manager => generateManagerCard(manager))
     )
 
+    const generateEngineerCard = engineer => {
+        return
+        <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-header">
+        Engineer
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">${engineer.name}</h5>
+        <a href="mailto:${engineer.email}" class="btn btn-primary">Email Me</a>
+        <a href="https://www.github.com/${engineer.github}" class="btn btn-primary">My Github</a>
+      </div>
+      <div class="card-footer text-muted">
+      ${engineer.id}
+    </div>
+    </div>
+  </div>
+    </div>
+    }
+
+    html.push(team.filter(employee => employee.getRole() === "Engineer")
+    .map(manager => generateEngineerCard(engineer))
+    )
+
+    const generateInternCard = intern => {
+        return
+        <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-header">
+        Intern
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">${intern.name}</h5>
+        <p class="card-text">${intern.school}</p>
+        <a href="mailto:${intern.email}" class="btn btn-primary">Email Me</a>
+      </div>
+      <div class="card-footer text-muted">
+      ${intern.id}
+    </div>
+    </div>
+  </div>
+    </div>
+    }
+
+    html.push(team.filter(employee => employee.getRole() === "intern")
+    .map(manager => generateinternCard(engineer))
+    )
+
     return html.join("") 
 } 
+
+module.exports= team => {
+    return `
+    <!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+  <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <title>Team Builder</title>
+</head>
+${makeTeam(team)}`
+}
